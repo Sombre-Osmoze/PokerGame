5d0e652f0b2971e8a1ec04a3b0246a6daa61a044
Marcus Florentin <marcus@osmoze.xyz>
2021-10-18 07:02:23 +0200

    Adding all tests for linux

--- Tests/PokerGameTests/CardsTests.swift
+++ Tests/PokerGameTests/CardsTests.swift
@@ -18,4 +18,13 @@ class CardsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+
+    func testExample() {
+            //
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+
 }
